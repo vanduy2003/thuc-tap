e8127d421edd905049eef339db39db3c40d0301d
Your Name <you@example.com>
2023-06-24 20:38:30 +0700

    axcs

--- json.js
+++
@@ -1,8 +0,0 @@
-var postApi = 'https://docs.google.com/spreadsheets/d/1rCoaE4pJ_YqwIxreaiorsXZ-vHpnXIc4ZuTFPQLIM-s/edit#gid=1243951575'
-fetch(postApi)
-    .then(function(reponse) {
-        return reponse.json();
-    })
-    .then(function(posts) {
-        console.log(posts);
-    });
